de24896540f50c43213220938cb3b45146acb4a3
JesseKerkvliet <jesse_kerkvliet@hotmail.com>
2015-01-05 13:56:57 +0100

    Update Bmolscript.py

--- Bmolscript.py
+++ Bmolscript.py
@@ -1,3 +1,10 @@
+def inputs():
+  sequentie = str(raw_input("Voer hier de sequentie in: ")
+  begin = int(raw_input("Voer hier het beginpunt in waar de primers mogen annealen: ")
+  eind = int(raw_input("Voer hier het beginpunt in waar de primers mogen annealen: ")
+  max_grootte = int(raw_input("Hoe groot mag het PCR product maximaal zijn? ")
+
+
 def positie_pakker(pp_sequentie):
   beginpos = 4
   eindpos = 10
