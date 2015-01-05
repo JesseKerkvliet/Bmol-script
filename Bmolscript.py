3fe0dd425a6dc16ecfbb750d964fa6cbe6317e7f
JesseKerkvliet <jesse_kerkvliet@hotmail.com>
2015-01-05 13:30:30 +0100

    Create Bmolscript.py
    Het script dat de primers nakijkt

---
+++ Bmolscript.py
@@ -0,0 +1,15 @@
+def sequentie_setter():
+  seqlijst = []
+  sequentie = raw_input("Vul hier een sequentie in: ")
+  for letter in sequentie:
+    seqlijst.append(letter)
+  print seqlijst
+  return seqlijst 
+def positie_pakker():
+  beginpos = 1
+  eindpos = 100
+  
+def main():
+    seqlijst = sequentie_setter()
+    positie_pakker(seqlijst)
+main()
